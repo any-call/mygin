aa383544531b5f088fc780deb9b6498eb93a3584
jinguihua <156711203@qq.com>
2024-06-26 09:45:04 +0800

    changed

--- core.go
+++ core.go
@@ -15,7 +15,7 @@ func Cors() gin.HandlerFunc {
 		c.Writer.Header().Set("Allow", "POST, OPTIONS,DELETE,GET, PUT")
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Access-Control-Allow-Origin,Access-Control-Allow-Headers,token, x-requested-with")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin,Content-Type,Content-Length,Accept-Encoding,Authorization,X-CSRF-Token,Access-Control-Allow-Origin,Access-Control-Allow-Headers,token, x-requested-with")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS,DELETE, GET, PUT")
 		if c.Request.Method == http.MethodOptions || c.Request.Method == http.MethodHead {
 			c.AbortWithStatus(http.StatusOK)

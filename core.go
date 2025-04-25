9d5fe0fca72bc88b3d36bbf863a164817c956345
jinguihua <156711203@qq.com>
2025-04-25 14:35:27 +0800

    changed

--- core.go
+++ core.go
@@ -1,8 +1,13 @@
 package mygin
 
 import (
+	"fmt"
+	"github.com/any-call/gobase/util/mycrypto"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 	"net/http"
+	"sort"
+	"strings"
 )
 
 var engine = gin.New()
@@ -23,4 +28,94 @@ func Cors() gin.HandlerFunc {
 		}
 		c.Next()
 	}
+}
+
+func AuthSign(signKey string, secretKey string,
+	getParamCb func(ctx *gin.Context) map[string]string,
+	postParamCb func(ctx *gin.Context) map[string]string,
+	putParamCb func(ctx *gin.Context) map[string]string,
+	deleteParamCb func(ctx *gin.Context) map[string]string,
+) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 获取所有请求参数（支持 GET 和 POST）
+		var paramMap = make(map[string]string, 0)
+		var err error
+		switch c.Request.Method {
+		case "GET":
+			if getParamCb != nil {
+				paramMap = getParamCb(c)
+			} else {
+				err = c.ShouldBindQuery(&paramMap)
+			}
+			break
+		case "POST":
+			if postParamCb != nil {
+				paramMap = postParamCb(c)
+			} else {
+				err = c.ShouldBindWith(&paramMap, binding.Form)
+			}
+			break
+		case "PUT":
+			if putParamCb != nil {
+				paramMap = putParamCb(c)
+			} else {
+				err = c.ShouldBindWith(&paramMap, binding.Form)
+			}
+			break
+		case "DELETE":
+			if deleteParamCb != nil {
+				paramMap = deleteParamCb(c)
+			} else {
+				err = c.ShouldBindBodyWith(&paramMap, FormUrlEncode)
+			}
+			break
+		}
+
+		if paramMap == nil || len(paramMap) == 0 || err != nil {
+			c.Next()
+			return
+		}
+
+		signValue, ok := paramMap[signKey]
+		if !ok {
+			c.String(http.StatusOK, `{"code":402,"msg":"missing sign"}`)
+			c.Abort()
+			return
+		}
+
+		delete(paramMap, signValue)
+		computedSign := GetAuthSign(paramMap, secretKey)
+		if computedSign != signValue {
+			c.String(http.StatusOK, `{"code":402,"msg":"invalid sign"}`)
+			c.Abort()
+			return
+		}
+
+		// 验签通过，继续处理
+		c.Next()
+	}
+}
+
+// GenerateSign 接收参数 map 和密钥，生成签名字符串（MD5）
+func GetAuthSign(params map[string]string, secretKey string) string {
+	// 提取 key 并排序
+	keys := make([]string, 0, len(params))
+	for k := range params {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	// 构造 key=value&key2=value2 的格式
+	var builder strings.Builder
+	for i, k := range keys {
+		if i < len(keys)-1 {
+			builder.WriteString(fmt.Sprintf("%s=%s&", k, params[k]))
+		} else {
+			builder.WriteString(fmt.Sprintf("%s=%s", k, params[k]))
+		}
+	}
+
+	// 加上 secretKey
+	rawString := builder.String() + secretKey
+	return mycrypto.Md5(rawString)
 }
